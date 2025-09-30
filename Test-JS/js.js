b2c906930c643d25ffe5dc30fb77288c3026d464
kiyanamiiii <enzoanami@gmail.com>
2025-09-30 09:06:37 -0300

    feat: simple js which create a button and a text below when cilck on the button

---
+++ Test-JS/js.js
@@ -0,0 +1,18 @@
+btn = document.createElement("button");
+btn.id = "btn";
+btn.innerText = "Clica ae faz favor";
+
+prg = document.createElement("p");
+prg.id = "prg";
+
+document.body.appendChild(btn);
+document.body.appendChild(prg);
+
+document.body.style.height = "100vh";
+document.body.style.display = "flex";
+document.body.style.justifyContent = "center";
+document.body.style.alignItems = "center";
+document.body.style.flexDirection = "column";
+
+addEventListener;
+btn.addEventListener("click", () => (prg.innerText = "Safadeza"));
